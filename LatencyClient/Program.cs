2b355a09410fb35add49a3530c543e6a5b7c79fb
Mike Harder <mharder@microsoft.com>
2018-05-03 18:05:49 -0700

    Server and client apps to measure Kestrel latency with slow responses

---
+++ LatencyClient/Program.cs
@@ -0,0 +1,88 @@
+﻿using CommandLine;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LatencyClient
+{
+    class Options
+    {
+        [Option('u', "url", Required = true)]
+        public string Url { get; set; }
+
+        [Option('r', "rps", Default = 10)]
+        public int RequestsPerSecond { get; set; }
+    }
+
+    class Program
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        static Task<int> Main(string[] args)
+        {
+            return Parser.Default.ParseArguments<Options>(args).MapResult(
+                options => Run(options),
+                _ => Task.FromResult(1)
+            );
+        }
+
+        private static async Task<int> Run(Options options)
+        {
+            var requestTracker = new RequestTracker();
+
+            await Task.WhenAll(
+                PrintResults(requestTracker),
+                ExecuteRequests(options.Url, options.RequestsPerSecond, requestTracker));
+
+            return 0;
+        }
+
+        private static async Task PrintResults(RequestTracker requestTracker)
+        {
+            var last = DateTime.MinValue;
+            while (true)
+            {
+                var now = DateTime.Now;
+
+                var (requestsStarted, requestsCompleted, averageLatency,
+                     recentRequestsStarted, recentRequestsCompleted, recentAverageLatency) = requestTracker.GetSummary(last, now);
+
+                Console.WriteLine($"Total: {requestsStarted}/{requestsCompleted}/{Math.Round(averageLatency.TotalMilliseconds)}" +
+                    $", Recent: {recentRequestsStarted}/{recentRequestsCompleted}/{Math.Round(recentAverageLatency.TotalMilliseconds)}");
+
+                last = now;
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        private static async Task ExecuteRequests(string url, int requestsPerSecond, RequestTracker requestTracker)
+        {
+            var start = DateTime.Now;
+
+            while (true)
+            {
+                var now = DateTime.Now;
+                var elapsed = now - start;
+                var expectedRequestsStarted = requestsPerSecond * elapsed.TotalSeconds;
+                var requestsStarted = requestTracker.RequestsStarted;
+
+                if (requestsStarted < expectedRequestsStarted)
+                {
+                    requestTracker.RequestStarted();
+                    ExecuteRequest(url, requestTracker);
+                }
+                else
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(1 / requestsPerSecond));
+                }
+            }
+        }
+
+        private static async Task ExecuteRequest(string url, RequestTracker requestTracker)
+        {
+            var requestStarted = DateTime.Now;
+            await _httpClient.GetAsync(url);
+            requestTracker.CompleteRequest(DateTime.Now - requestStarted);
+        }
+    }
+}
---
+++ LatencyClient/RequestTracker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LatencyClient
+{
+    class RequestTracker
+    {
+        private ConcurrentStack<DateTime> _requestsStarted = new ConcurrentStack<DateTime>();
+        private ConcurrentStack<(DateTime Completed, TimeSpan Latency)> _requestsCompleted =
+            new ConcurrentStack<(DateTime Completed, TimeSpan Latency)>();
+
+        public (int TotalRequestsStarted, int TotalRequestsCompleted, TimeSpan TotalAverageLatency,
+                int SelectedRequestsStarted, int SelectedRequestsCompleted, TimeSpan SelectedAverageLatency)
+            GetSummary(DateTime startInclusive, DateTime endExclusive)
+        {
+            var selectedRequestsStarted = _requestsStarted.Where(d => d >= startInclusive && d < endExclusive);
+            var selectedRequestsCompleted = _requestsCompleted.Where(d => d.Completed >= startInclusive && d.Completed < endExclusive);
+
+            var totalAverageLatency = _requestsCompleted.Count() > 0 ?
+                TimeSpan.FromTicks((long)_requestsCompleted.Select(r => r.Latency.Ticks).Average()) :
+                TimeSpan.Zero;
+
+            var selectedAverageLatency = selectedRequestsCompleted.Count() > 0 ?
+                TimeSpan.FromTicks((long)selectedRequestsCompleted.Select(r => r.Latency.Ticks).Average()) :
+                TimeSpan.Zero;
+
+            return (
+                _requestsStarted.Count(),
+                _requestsCompleted.Count(),
+                totalAverageLatency,
+                selectedRequestsStarted.Count(),
+                selectedRequestsCompleted.Count(),
+                selectedAverageLatency
+                );
+        }
+
+        public int RequestsStarted => _requestsStarted.Count;
+
+        public void RequestStarted()
+        {
+            _requestsStarted.Push(DateTime.Now);
+        }
+
+        public void CompleteRequest(TimeSpan latency)
+        {
+            _requestsCompleted.Push((DateTime.Now, latency));
+        }
+    }
+}
